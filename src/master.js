e53cf5bd3a54ebefb168178960b71ad3c98bd0e7
minemidnight <minemidnightmc@gmail.com>
2017-08-16 14:33:20 -0400

    wait for workers to spawn before spawning next

--- src/master.js
+++ src/master.js
@@ -1,27 +1,31 @@
 const fs = Promise.promisifyAll(require("fs"));
 let port = 7500;
 
-async function spawnApiVersions() {
-	let apiVersions = await fs.readdirAsync(`${__dirname}/api/`);
-	for(let version of apiVersions) {
-		version = version.substring(1);
-		let worker = cluster.fork();
-		worker.once("online", () => {
-			worker.send({ type: "startup", processType: "api", version, port });
-			port++;
-		});
-	}
-}
-
 async function init() {
 	await (require(`${__dirname}/rethink`)).init();
 
 	let website = cluster.fork();
-	website.once("online", () => {
-		website.send({ type: "startup", processType: "website", port });
-		port++;
+	await new Promise(resolve => {
+		website.once("online", () => {
+			website.send({ type: "startup", processType: "website", port });
+			port++;
 
-		spawnApiVersions();
+			resolve(true);
+		});
 	});
+
+	let apiVersions = await fs.readdirAsync(`${__dirname}/api/`);
+	for(let version of apiVersions) {
+		version = version.substring(1);
+		let worker = cluster.fork();
+		await new Promise(resolve => {
+			website.once("online", () => {
+				worker.send({ type: "startup", processType: "api", version, port });
+				port++;
+
+				resolve(true);
+			});
+		});
+	}
 }
 init();
