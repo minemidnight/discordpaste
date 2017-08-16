a15a6685a5ea6eb7740d5d98c52ea37c29ec4b2f
minemidnight <minemidnightmc@gmail.com>
2017-08-16 01:05:33 -0400

    fix port adding

--- src/master.js
+++ src/master.js
@@ -5,15 +5,18 @@ async function init() {
 	await (require(`${__dirname}/rethink`)).init();
 
 	let website = cluster.fork();
-	website.once("online", () => website.send({ type: "startup", processType: "website", port }));
-	port++;
+	website.once("online", () => {
+		website.send({ type: "startup", processType: "website", port });
+	});
 
 	let apiVersions = await fs.readdirAsync(`${__dirname}/api/`);
 	for(let version of apiVersions) {
 		version = version.substring(1);
 		let worker = cluster.fork();
-		worker.once("online", () => worker.send({ type: "startup", processType: "api", version, port }));
-		port++;
+		worker.once("online", () => {
+			worker.send({ type: "startup", processType: "api", version, port });
+			port++;
+		});
 	}
 }
 init();
