4a889cfd6d6eec4595a73f04e4d6097e5ca3c60e
minemidnight <minemidnightmc@gmail.com>
2017-08-16 00:55:57 -0400

    small code fixes

--- src/worker.js
+++ src/worker.js
@@ -1,12 +1,15 @@
-require("./rethink");
 async function startupMessage(msg) {
 	if(msg.type !== "startup") {
 		cluster.worker.once("message", startupMessage);
 		return;
 	}
 
-	if(msg.processType === "api") require(`${__dirname}/api/v${msg.version}/index.js`)(msg.port);
-	else require(`${__dirname}/site/index.js`)(msg.port);
+	if(msg.processType === "api") {
+		require(`${__dirname}/rethink`);
+		require(`${__dirname}/api/v${msg.version}/index.js`)(msg.port);
+	} else {
+		require(`${__dirname}/site/index.js`)(msg.port);
+	}
 }
 
 cluster.worker.once("message", startupMessage);
