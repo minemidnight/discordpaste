f79c754751889b2135bd9c00e84c74358431ac92
minemidnight <minemidnightmc@gmail.com>
2017-08-16 01:20:59 -0400

    small fix but still bork

--- src/site/public/assets/js/main.js
+++ src/site/public/assets/js/main.js
@@ -1,8 +1,9 @@
 const superagent = require("superagent");
-const editor = CodeMirror.fromTextArea($("#editor")[0], { // eslint-disable-line no-undef
-	lineNumbers: true,
-	indentWithTabs: true,
-	tabSize: 2,
-	autofocus: true
+$(window).onload(() => {
+	const editor = CodeMirror.fromTextArea($("#editor")[0], { // eslint-disable-line no-undef
+		lineNumbers: true,
+		indentWithTabs: true,
+		tabSize: 2,
+		autofocus: true
+	});
 });
-
