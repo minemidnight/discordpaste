bcc4f6a32c3f01ffe6a217e52005936670897740
minemidnight <minemidnightmc@gmail.com>
2017-08-16 14:48:33 -0400

    fix window onload

--- src/site/public/assets/js/main.js
+++ src/site/public/assets/js/main.js
@@ -1,5 +1,5 @@
 const superagent = require("superagent");
-$(window).onload(() => {
+$(window).on("load", () => {
 	const editor = CodeMirror.fromTextArea($("#editor")[0], { // eslint-disable-line no-undef
 		lineNumbers: true,
 		indentWithTabs: true,
