ce0f7e388cd5e31abd35914382850ab50d9d7a19
minemidnight <minemidnightmc@gmail.com>
2017-08-18 13:21:43 -0400

    make new document not reload page

--- src/site/public/assets/js/view.js
+++ src/site/public/assets/js/view.js
@@ -10,7 +10,31 @@ $(window).on("load", () => {
 		readOnly: true
 	});
 
-	$("#new").on("click", () => window.location.assign(window.location.origin));
+	$("#new").on("click", () => {
+		document.title = "DiscordPaste";
+		window.history.pushState("", "", "/");
+		editor.setOption("readOnly", false);
+		editor.setValue("");
+		editor.clearHistory();
+
+		$("#duplicate").addClass("disabled");
+		$("#raw").addClass("disabled");
+		$("#buttons").children().off("click");
+
+		$("#save").on("click", async () => {
+			let content = editor.getValue();
+			if(!content) return;
+
+			let { body: { id } } = await superagent.post(`${window.location.origin}/api/v1/documents`).send({ content });
+			window.location.assign(`${window.location.origin}/${id}`);
+		});
+
+		$("#new").on("click", () => {
+			editor.setValue("");
+			editor.clearHistory();
+		});
+	});
+
 	$("#duplicate").on("click", () => {
 		document.title = "DiscordPaste";
 		window.history.pushState("", "", "/");
