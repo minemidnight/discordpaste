ebb9948fed75a2a38fb6772d7c08840ff2a7a2f6
minemidnight <minemidnightmc@gmail.com>
2017-08-19 13:22:12 -0400

    send content type header for result

--- src/site/routes/raw.js
+++ src/site/routes/raw.js
@@ -4,6 +4,7 @@ const superagent = require("superagent");
 router.get("/:id", async (req, res) => {
 	try {
 		let { body: { content } } = await superagent.get(`${app.config.baseURL}/api/v1/documents/${req.params.id}`);
+		res.set("Content-Type", "text/plain");
 		res.status(200).send(content).end();
 	} catch(err) {
 		res.redirect(app.config.baseURL);
