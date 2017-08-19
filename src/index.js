ce23f4d0da7080f0f5abd9120b42ab78bfab1d64
minemidnight <minemidnightmc@gmail.com>
2017-08-19 00:56:52 -0400

    log to stderr instead of stdout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 global.Promise = require("bluebird");
 global.cluster = require("cluster");
 global.express = require("express");
-process.on("unhandledRejection", err => console.log(err.stack));
+process.on("unhandledRejection", err => console.error(err.stack));
 if(cluster.isMaster) require("./master.js");
 else require("./worker.js");
