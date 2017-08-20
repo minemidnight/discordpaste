1734eb1d54dc24add7da5f46c37cc003632a1e54
minemidnight <minemidnightmc@gmail.com>
2017-08-20 18:49:07 -0400

    fix valid lang

--- src/misc/validLang.js
+++ src/misc/validLang.js
@@ -4,15 +4,16 @@ let languageMap = Object.keys(languages)
 	.filter(key => languages[key].codemirrorMode && languages[key].extensions)
 	.reduce((prev, key) => {
 		let data = languages[key];
-		data.extensions.forEach(ext => prev[ext.substring(1)] = data.codemirrorMode);
-		prev[data.codemirrorMode] = data.extensions[0].substirng(1);
+		data.extensions.forEach(ext => prev.exts[ext.substring(1)] = data.codemirrorMode);
+		prev.modes[data.codemirrorMode] = data.extensions[0].substirng(1);
 		return prev;
-	}, {});
+	}, { modes: {}, exts: {} });
 
 module.exports = lang => {
-	lang = languageMap[lang];
-	if(!lang) return false;
+	let mapValue = languageMap.exts[lang], extension;
+	if(!mapValue) lang = extension = languageMap.modes[lang];
+	else extension = languageMap.modes[mapValue];
 
-	let extension = typeof lang === "string" ? lang : languageMap[lang];
+	if(!lang) return false;
 	return { codemirror: lang, extension };
 };
