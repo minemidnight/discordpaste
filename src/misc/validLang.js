fa761e8a293febcae3f0a8bfe94e232f51dbbbeb
minemidnight <minemidnightmc@gmail.com>
2017-08-20 18:41:23 -0400

    fix extensions

--- src/misc/validLang.js
+++ src/misc/validLang.js
@@ -4,8 +4,8 @@ let languageMap = Object.keys(languages)
 	.filter(key => languages[key].codemirrorMode && languages[key].extensions)
 	.reduce((prev, key) => {
 		let data = languages[key];
-		data.extensions.forEach(ext => prev[ext] = data.codemirrorMode);
-		prev[data.codemirrorMode] = data.extensions[0];
+		data.extensions.forEach(ext => prev[ext.substring(1)] = data.codemirrorMode);
+		prev[data.codemirrorMode] = data.extensions[0].substirng(1);
 		return prev;
 	}, {});
 
